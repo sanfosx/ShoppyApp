a84556c8569c9829e2f7852c8faee59c16baf9af
Andres Bianchi <andresbianchi33@gmail.com>
2023-11-08 21:49:41 -0300

    agrego el lorem

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -4,6 +4,7 @@ const AboutPage = () => {
   return (
     <div>
       <h1>About Page</h1>
+      <h1>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel voluptatum ab culpa animi, magni ex aliquid, voluptates, nesciunt quae dolor molestiae assumenda? Ex laudantium in sapiente aut dignissimos. Nam, dolorum.</h1>
     </div>
   )
 }
