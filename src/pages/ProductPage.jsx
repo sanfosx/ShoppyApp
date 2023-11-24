8cfcba8f39ecb2eba4fcdd8e525a2595d73269d3
Jfrattin <106035011+Jfrattin@users.noreply.github.com>
2023-11-24 12:09:25 -0300

    Agrego cambios a la pagina de productpage

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -2,7 +2,7 @@
 import { useNavigate, useParams } from 'react-router-dom'
 import { useQuery } from 'react-query';
 import {PlatziAPI} from '../../src/data/ApiPlatzi';
-
+import { Card, CardBody, CardHeader, CardImg, CardText, Col, Row , Spinner, Badge , Stack} from 'react-bootstrap';
 
 const ProductPage = () => {
     const navigate = useNavigate()
@@ -12,7 +12,9 @@ const ProductPage = () => {
 
 
     if (isLoading) {
-        return <div>Cargando...</div>;
+        return <div><Spinner animation="border" variant="primary" role="status">
+        <span className="visually-hidden">Loading...</span>
+        </Spinner></div>;
     }
 
     if (isError) {
@@ -23,17 +25,37 @@ const ProductPage = () => {
         <div className='container-content'>
 
             {data ?
-                <div key={data.id}>
-                    <button className="btn btn-dark" onClick={() => navigate(-1)}>Volver atras</button>
-                    <h1> {id}- {data.title} </h1>
-                    <img src={data.images[0]} alt="" className="rounded mx-auto d-block" />
-                    <h2>$ {data.price}</h2>
-                    <p>{data.description}</p>
-                    <p>Creado el : {data.creationAt}</p>
-                    <p>Ultima Actualizacion : {data.updatedAt}</p>
-                    <button className="btn btn-dark" onClick={() => navigate(-1)}>Volver atras</button>
-                   
-                </div>
+                <Card className="ml-auto mt-2" >
+                <Card.Title className="m-2">
+                        <row className="d-flex justify-content-end ">
+                        <button className="btn btn-dark" onClick={() => navigate(-1)}>Volver atras</button>
+                        </row>
+                </Card.Title>
+                <CardBody>
+                    
+                    <div className='d-flex '>
+
+                        <CardImg  className="m-1" src= {data.images[0]} style={{ width: '50%' }}></CardImg>
+                        <CardText className="d-flex flex-column m-1" style={{ width: '50%' }}>
+                            <Card> 
+                            <row>  
+                            <h1>{data.title}</h1> 
+                            <Stack direction="horizontal" gap={2}>
+                            <Badge bg="info" text="dark">{data.category.name}</Badge>
+                            </Stack>
+                            <h2 className='text-right'>Price: </h2>
+                            <h2>$ {data.price}</h2>
+                            </row>
+                            <h3>Descriptions:</h3>
+                            <h4>{data.description}</h4>
+                            <button className="btn btn-primary">Lo Quiero</button>
+                            </Card> 
+
+                        </CardText>
+                    </div>
+                 
+                    </CardBody>
+                </Card>
                 : <></>
             }
         </div>
