1971c123b0794f4d96651f56ea94e502ad3a8f95
AlvaroGuiffrey <alvaroguiffrey@gmail.com>
2023-12-09 15:24:37 -0300

    AboutPages agrego cards

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -2,10 +2,213 @@
 
 const AboutPage = () => {
   return (
-    <div>
-      <h1>About Page</h1>
-      <h1>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel voluptatum ab culpa animi, magni ex aliquid, voluptates, nesciunt quae dolor molestiae assumenda? Ex laudantium in sapiente aut dignissimos. Nam, dolorum.</h1>
+    <div className="container-fluid p-1">
+
+      <h1>Quienes somos</h1>
+      <p>Somos el Grupo 6 de la Comisión 23643 que cursamos JS React 2023 del programa Codo a Codo de la Agencia de Aprendizaje
+        a lo largo de la vida del Ministerio de Eduación del Gobierno de la Ciudad de Buenos Aires.
+      </p>
+      <h1>Integrantes</h1>
+      <div className="container-fluid">
+        <div className="row">
+          {/* Card de integrante 1 */}
+          <div className="col-sm-3 p-2 mb-2">
+            <div className="card p-2">
+              {/* Foto del integrante */}
+              <img className="card-image w-50 mx-auto rounded-3" 
+                src="../../public/image/sinImagen.png" 
+                alt="Victor Andrés Bianchi Núñez" />
+              {/* Detalle del integrante */}
+              <div className="card-body">
+                <h5 className="card-title text-center">Victor Andrés Bianchi Núñez</h5>
+                <p className="card-text">Alumno.</p>
+              </div>
+            </div>
+          </div>
+          {/* Fin Card */}
+
+          {/* Card de integrante 2 */}
+          <div className="col-sm-3 p-2 mb-2">
+            <div className="card p-2">
+              {/* Foto del integrante */}
+              <img className="card-image w-50 mx-auto rounded-3" 
+                src="../../public/image/sinImagen.png" 
+                alt="Iván Bustos" />
+              {/* Detalle del integrante */}
+              <div className="card-body">
+                <h5 className="card-title text-center">Iván Bustos</h5>
+                <p className="card-text">Alumno.</p>
+              </div>
+            </div>
+          </div>
+          {/* Fin Card */}
+
+          {/* Card de integrante 3 */}
+          <div className="col-sm-3 p-2 mb-2">
+            <div className="card p-2">
+              {/* Foto del integrante */}
+              <img className="card-image w-50 mx-auto rounded-3" 
+                src="../../public/image/sinImagen.png" 
+                alt="Martín Alejandro Caro" />
+              {/* Detalle del integrante */}
+              <div className="card-body">
+                <h5 className="card-title text-center">Martín Alejandro Caro</h5>
+                <p className="card-text">Alumno.</p>
+              </div>
+            </div>
+          </div>
+          {/* Fin Card */}
+
+          {/* Card de integrante 4 */}
+          <div className="col-sm-3 p-2 mb-2">
+            <div className="card p-2">
+              {/* Foto del integrante */}
+              <img className="card-image w-50 mx-auto rounded-3" 
+                src="../../public/image/sinImagen.png" 
+                alt="Luciano Costantini" />
+              {/* Detalle del integrante */}
+              <div className="card-body">
+                <h5 className="card-title text-center">Luciano Costantini</h5>
+                <p className="card-text">Alumno.</p>
+              </div>
+            </div>
+          </div>
+          {/* Fin Card */}
+
+          {/* Card de integrante 5 */}
+          <div className="col-sm-3 p-2 mb-2">
+            <div className="card p-2">
+              {/* Foto del integrante */}
+              <img className="card-image w-50 mx-auto rounded-3" 
+                src="../../public/image/sinImagen.png" 
+                alt="Santiago Saul Foschiatti" />
+              {/* Detalle del integrante */}
+              <div className="card-body">
+                <h5 className="card-title text-center">Santiago Saul Foschiatti</h5>
+                <p className="card-text">Alumno.</p>
+              </div>
+            </div>
+          </div>
+          {/* Fin Card */}
+
+          {/* Card de integrante 6 */}
+          <div className="col-sm-3 p-2 mb-2">
+            <div className="card p-2">
+              {/* Foto del integrante */}
+              <img className="card-image w-50 mx-auto rounded-3" 
+                src="../../public/image/sinImagen.png" 
+                alt="Joaquin Frattin" />
+              {/* Detalle del integrante */}
+              <div className="card-body">
+                <h5 className="card-title text-center">Joaquin Frattin</h5>
+                <p className="card-text">Alumno.</p>
+              </div>
+            </div>
+          </div>
+          {/* Fin Card */}
+
+          {/* Card de integrante 7 */}
+          <div className="col-sm-3 p-2 mb-2">
+            <div className="card p-2">
+              {/* Foto del integrante */}
+              <img className="card-image w-50 mx-auto rounded-3" 
+                src="../../public/image/alvaroGuiffrey.jpg" 
+                alt="Alvaro Guiffrey" />
+              {/* Detalle del integrante */}
+              <div className="card-body">
+                <h5 className="card-title text-center">Alvaro Alejandro Guiffrey</h5>
+                <p className="card-text">Alumno. Programador PHP, Python, JS.</p>
+              </div>
+            </div>
+          </div>
+          {/* Fin Card */}
+
+          {/* Card de integrante 8 */}
+          <div className="col-sm-3 p-2 mb-2">
+            <div className="card p-2">
+              {/* Foto del integrante */}
+              <img className="card-image w-50 mx-auto rounded-3" 
+                src="../../public/image/sinImagen.png" 
+                alt="Julian Meier" />
+              {/* Detalle del integrante */}
+              <div className="card-body">
+                <h5 className="card-title text-center">Julian Meier</h5>
+                <p className="card-text">Alumno.</p>
+              </div>
+            </div>
+          </div>
+          {/* Fin Card */}
+
+          {/* Card de integrante 9 */}
+          <div className="col-sm-3 p-2 mb-2">
+            <div className="card p-2">
+              {/* Foto del integrante */}
+              <img className="card-image w-50 mx-auto rounded-3" 
+                src="../../public/image/sinImagen.png" 
+                alt="Daniel Ernesto Obregon" />
+              {/* Detalle del integrante */}
+              <div className="card-body">
+                <h5 className="card-title text-center">Daniel Ernesto Obregon</h5>
+                <p className="card-text">Alumno.</p>
+              </div>
+            </div>
+          </div>
+          {/* Fin Card */}
+
+          {/* Card de integrante 10 */}
+          <div className="col-sm-3 p-2 mb-2">
+            <div className="card p-2">
+              {/* Foto del integrante */}
+              <img className="card-image w-50 mx-auto rounded-3" 
+                src="../../public/image/sinImagen.png" 
+                alt="Pedro Raul Rios" />
+              {/* Detalle del integrante */}
+              <div className="card-body">
+                <h5 className="card-title text-center">Pedro Raul Rios</h5>
+                <p className="card-text">Alumno.</p>
+              </div>
+            </div>
+          </div>
+          {/* Fin Card */}
+
+          {/* Card de integrante 11 */}
+          <div className="col-sm-3 p-2 mb-2">
+            <div className="card p-2">
+              {/* Foto del integrante */}
+              <img className="card-image w-50 mx-auto rounded-3" 
+                src="../../public/image/sinImagen.png" 
+                alt="Sebastián Rucci" />
+              {/* Detalle del integrante */}
+              <div className="card-body">
+                <h5 className="card-title text-center">Sebastián Rucci</h5>
+                <p className="card-text">Alumno.</p>
+              </div>
+            </div>
+          </div>
+          {/* Fin Card */}
+
+          {/* Card de integrante 12 */}
+          <div className="col-sm-3 p-2 mb-2">
+            <div className="card p-2">
+              {/* Foto del integrante */}
+              <img className="card-image w-50 mx-auto rounded-3" 
+                src="../../public/image/sinImagen.png" 
+                alt="Facundo Tognola" />
+              {/* Detalle del integrante */}
+              <div className="card-body">
+                <h5 className="card-title text-center">Facundo Tognola</h5>
+                <p className="card-text">Alumno.</p>
+              </div>
+            </div>
+          </div>
+          {/* Fin Card */}
+
+
+        </div>
+      </div>
+
     </div>
+
   )
 }
 
