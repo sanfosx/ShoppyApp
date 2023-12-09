96e5ed4a1a2fbcf63fd7f7e96431c9d550be8188
AlvaroGuiffrey <alvaroguiffrey@gmail.com>
2023-12-09 10:16:16 -0300

    Footer - Modifica centrado

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,14 +1,11 @@
 
-
 const Footer = () => {
   return (
-  
-      <footer >
-        <div className=" container-fluid d-flex align-items-center">
-          <span className="mb-3 mb-md-0 text-body-secondary p-3 text-center">&copy; Codo a Codo React 2023 Comision 23643 Grupo 6</span>
-        </div>
-
-      </footer>
+    <footer >
+      <div className="container-fluid d-flex  justify-content-center">
+        <span className="mb-3 mb-md-0 text-body-secondary p-3 text-center">&copy; Codo a Codo React 2023 Comision 23643 Grupo 6</span>
+      </div>
+    </footer>
   )
 }
 
